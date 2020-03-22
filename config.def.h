692d891daa8193e4e36aadefb26555114958925d
egidius <egidius@mysliwietz.de>
2020-03-23 00:13:06 +0100

    Testing even fyra layout

--- config.def.h
+++ config.def.h
@@ -44,9 +44,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.8; /* factor of master area size [0.05..0.95] */
-static const float smfact     = 0.2; /* factor of tiled clients [0.00..0.95] */
-static const float shfact     = 0.2; /* factor of tertiary tiled clients [0.00..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const float smfact     = 0.5; /* factor of tiled clients [0.00..0.95] */
+static const float shfact     = 0.5; /* factor of tertiary tiled clients [0.00..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
